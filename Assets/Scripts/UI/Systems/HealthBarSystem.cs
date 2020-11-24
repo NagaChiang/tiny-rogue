0b223931382bd05d42c2d7e73660637ca6b3e41c
Naga Chiang <nagachiangdev@gmail.com>
2020-11-25 00:08:29 +0800

    Remove redundant component from entity

--- Assets/Scripts/UI/Systems/HealthBarSystem.cs
+++ Assets/Scripts/UI/Systems/HealthBarSystem.cs
@@ -10,19 +10,19 @@ namespace Timespawn.TinyRogue.UI
         protected override void OnUpdate()
         {
             ComponentDataFromEntity<HealthBar> healthBarFromEntity = GetComponentDataFromEntity<HealthBar>(true);
-            ComponentDataFromEntity<NonUniformScale> scaleFromEntity = GetComponentDataFromEntity<NonUniformScale>(true);
 
             EndSimulationEntityCommandBufferSystem endSimECBSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
             EntityCommandBuffer.ParallelWriter parallelWriter = endSimECBSystem.CreateCommandBuffer().AsParallelWriter();
             Entities
                 .WithReadOnly(healthBarFromEntity)
-                .WithReadOnly(scaleFromEntity)
                 .WithChangeFilter<Health>()
                 .ForEach((int entityInQueryIndex, in Health health, in HealthBarLink healthBarLink) =>
                 {
                     HealthBar healthBar = healthBarFromEntity[healthBarLink.Value];
-                    NonUniformScale scale = scaleFromEntity[healthBar.BarEntity];
-                    scale.Value = new float3((float) health.Current / health.Max, 1.0f, 1.0f);
+                    NonUniformScale scale = new NonUniformScale
+                    {
+                        Value = new float3((float) health.Current / health.Max, 1.0f, 1.0f)
+                    };
 
                     parallelWriter.SetComponent(entityInQueryIndex, healthBar.BarEntity, scale);
                 }).ScheduleParallel();
